9edc594953251d6806e12050f6f4dbea7c8fe5da
Jose Neto <menezesneto13@hotmail.com>
2021-09-06 22:51:21 -0300

    ajustes

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ const cliente = new Client({
     host: 'ec2-44-196-8-220.compute-1.amazonaws.com',
     port: 5432,
     database: 'de6lt1r1jqdtkq',
-    ssl: false,
+    ssl: true,
     dialect: 'postgres',
     dialectOptions: {
-        "ssl": {"require":false }
+        "ssl": true
     }
 })
 
