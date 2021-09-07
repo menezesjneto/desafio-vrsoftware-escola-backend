8e4b149f809c29aa3be59c5bafc773d5488c71bd
Jose Neto <menezesneto13@hotmail.com>
2021-09-06 21:58:16 -0300

    Credenciais Heroku

--- index.js
+++ index.js
@@ -26,11 +26,16 @@ server.listen(3000, () =>{
 // ------------- Init DB -------------
 const Client = require('pg').Client
 const cliente = new Client({
-    user: 'postgres',
-    password: '123456',
-    host: '127.0.0.1',
+    // user: 'postgres',
+    // password: '123456',
+    // host: '127.0.0.1',
+    // port: 5432,
+    // database: 'escola'
+    user: 'ehoswfodeyibqz',
+    password: 'd91a21892451293ffe13c19df0baa91a4cd5a0b4d6b5319ec158d34d6e5bc4e1',
+    host: 'ec2-44-196-8-220.compute-1.amazonaws.com',
     port: 5432,
-    database: 'escola'
+    database: 'de6lt1r1jqdtkq'
 })
 
 // ------------- Run APIS -------------
