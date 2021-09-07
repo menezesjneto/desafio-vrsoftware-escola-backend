11b2624930bf8a3edf81d62bce7c0938a5ffe9df
Jose Neto <menezesneto13@hotmail.com>
2021-09-06 22:44:34 -0300

    Ajustes SSL.

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ const cliente = new Client({
     password: 'd91a21892451293ffe13c19df0baa91a4cd5a0b4d6b5319ec158d34d6e5bc4e1',
     host: 'ec2-44-196-8-220.compute-1.amazonaws.com',
     port: 5432,
-    database: 'de6lt1r1jqdtkq'
+    database: 'de6lt1r1jqdtkq',
+    ssl: true
 })
 
 // ------------- Run APIS -------------
